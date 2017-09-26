103f42842f7f1fb4acacfcbdfb280bc7f2f94481
Daniel Cook <danielecook@gmail.com>
2017-09-26 09:30:09 -0500

    Update packages

---
+++ install_r_packages.R
@@ -0,0 +1,13 @@
+#!/usr/env/Rscript --vanilla 
+
+# Install CRAN Packages
+install.packages(c(
+                   "devtools",
+                   "googlesheets",
+                   "geosphere",
+                   "leaflet",
+                   "rlang",
+                   "webshot",
+                   "knitr"
+                   )
+                )
