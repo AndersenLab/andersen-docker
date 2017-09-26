8607f8fae917d0ef4254d87255384a19b045b6ff
Daniel Cook <danielecook@gmail.com>
2017-09-26 11:58:58 -0500

    Add bioconductor packages

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,5 +1,37 @@
 #!/usr/env/Rscript --vanilla 
 
+source('https://bioconductor.org/biocLite.R')
+
+# Install biocLite packages
+biocLite(c("ape",
+  "ggdendro",
+  "ggbiplot",
+  "ggrepel",
+  "ggtree",
+  "PopGenome",
+  "qtlDesign",
+  "RColorBrewer",
+  "regress",
+  "rmarkdown",
+  "Rsamtools",
+  "RSVSim",
+  "sangerseqR",
+  "scales",
+  "seqinr",
+  "sjPlot",
+  "sjstats",
+  "sjmisc",
+  "simcross",
+  "ShortRead",
+  "SKAT",
+  "SparseM",
+  "survival",
+  "survsim",
+  "stringdist",
+  "viridis",
+  "zoo"), ask=F, suppressUpdates=T)
+
+
 # Install CRAN Packages
 install.packages(c(
                    "cowplot",
@@ -17,6 +49,7 @@ install.packages(c(
                    "roxygen2"
                    )
                 )
+
 
 devtools::install_github("Andersenlab/cegwas")
 devtools::install_github("Andersenlab/COPASutils")
